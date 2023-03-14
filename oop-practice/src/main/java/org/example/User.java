6bb67ccf7bb253589e579d0ccd7d69117cb8de9e
changwook lee <lcy12225@naver.com>
2023-03-15 05:29:19 +0900

    Test method with low coupling

---
+++ oop-practice/src/main/java/org/example/User.java
@@ -0,0 +1,30 @@
+package org.example;
+
+//import org.example.passwordTest.RandomPasswordGenerator;
+
+public class User {
+    private String password;
+
+    public void initPassword(PasswordGenerator passwordGenerator) {
+//        RandomPasswordGenerator randomPasswordGenerator = new RandomPasswordGenerator(); // 패키지가 변경되면 import를 헤줘야해서 강한결합 방식의 테스트
+        //String randomPassword = randomPasswordGenerator.generatePassword();
+
+
+        // interface를 구현함으로써 두가지의 테스트 방식을 진행가능. 느슨한 결합
+        String randomPassword = passwordGenerator.generatePassword();
+
+        int rp = randomPassword.length();
+
+        if(rp >= 8 && rp <= 12 ) {
+            this.password = randomPassword;
+        }
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
